799f3a46079a7bb70f06073abaf510b1c636ffb8
Shamimreza82 <shamimrezaone@gmail.com>
2023-12-21 16:15:23 +0600

    added navber

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
+
+import Navber from './Navber';
 
 const MainLayout = () => {
     return (
         <div>
-            <h1>main Lay Out</h1>
+            <Navber></Navber>
         </div>
     );
 };
